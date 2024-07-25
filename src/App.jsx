2ef6f865eccb1d814ae8cd0eeb57d9a8ccbe5234
shakil8055 <mohamedhshakil883@gmail.com>
2024-07-25 10:04:15 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,13 @@ import { Route, Routes } from 'react-router-dom';
 //pages
 import Signup from './pages/signup/Signup';
 import Login from './pages/login/Login';
-import Forgot from './pages/forgot/Forgot';
+import Forgot from './pages/forgot/Forgot'; 
 import Reset from './pages/reset/Reset';
-import ConfirmUser from './pages/confirmuser/ConfirmUser';
 import LoggedOut from './pages/loggedout/LoggedOut';
 import Roadmap from './pages/Roadmap/Roadmap';
-import Dashboard from './pages/Dashboard/Dashboard';
 import Tasks from './pages/Task/Tasks';
-import Requirements from './pages/Requirements/Requirements';
-import Application from './pages/Application/Application';
-import Interview from './pages/Interview/Interview';
-import Mockinterview from './pages/Mockinterview/Mockinterview';
-import Certificate from './pages/Certificate/Certificate';
-import Syllabus from './pages/Syllabus/Syllabus';
-import Leaderboard from './pages/Leaderboard/Leaderboard';
-import Portfolio from './components/Portfolio/Portfolio';
-import Webcode from './components/webcode/Webcode';
-import Capstone from './pages/capstone/Capstone';
-import Queries from './pages/Queries/Queries';
-import Leave from './pages/Leave/Leave';
+
+
 import DataContext from './context/DataContext';
 import Profile from './components/profile/Profile';
 import { ToastContainer } from 'react-toastify';
